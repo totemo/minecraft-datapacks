b570779cc15fbe18595736f6343d2ece59395ed6
totemo <TotemoResearch@gmail.com>
2018-06-30 19:40:02 +0930

    Set up build scripts as symlink of common build.sh.

--- coords/build.sh
+++ coords/build.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-HERE=$(cd $(dirname "$0") && pwd)
-PACKAGE=$(basename "$HERE")
-DIST="$HERE/dist"
-ARCHIVE="$DIST/$PACKAGE.zip"
-
-mkdir -p "$DIST"
-rm -f "$ARCHIVE"
-cd "$HERE/src/" && zip -r "$ARCHIVE" .
-
-echo "Created $ARCHIVE"
+../common/build.sh
--- waypoints/build.sh
+++ waypoints/build.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-HERE=$(cd $(dirname "$0") && pwd)
-PACKAGE=$(basename "$HERE")
-DIST="$HERE/dist"
-ARCHIVE="$DIST/$PACKAGE.zip"
-GENERATE="$HERE/generate.sh"
-
-# Generate code if generate.sh present.
-[ -x "$GENERATE" ] && "$GENERATE"
-
-mkdir -p "$DIST"
-rm -f "$ARCHIVE"
-cd "$HERE/src/" && zip -r "$ARCHIVE" .
-
-echo "Created $ARCHIVE"
+../common/build.sh
